49948f5431a810e44828c25e13703e4a50e14df7
Jirattikal42 <60840444+Jirattikal42@users.noreply.github.com>
2020-08-30 15:14:30 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -4,31 +4,47 @@ import WordCard from './WordCard';
 
 
 var word = ""; 
+var text = "";
 
 
 var rand = Math.floor(Math.random() * 4);
 switch (rand) {
-  case 0:  
+  case 0: 
+    text = 'จงเรียงคำว่า "โต๊ะ"'
     word = "table";
     break;
   case 1: 
+    text = 'จงเรียงคำว่า "เก้าอี้"'
     word = "chair";
     break;
   case 2:  
+    text = 'จงเรียงคำว่า "แก้ว"'
     word = "glass";
     break;
-  case 3:      
+  case 3:  
+    text = 'จงเรียงคำว่า "กระดาษ"'    
     word = "paper";
     break;
 }
 
 function App() {
   return (
-    <div>
-      {
-        <WordCard value={word}/>
-      }
-    </div>
+    <body>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1>{text}</h1>
+      <br></br>
+      <br></br>
+        <div class="cardlayout">
+          <WordCard value={word} />
+        </div>
+    </body>  
   );
 }
 
