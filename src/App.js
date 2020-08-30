ad7b9aa0b5e67fbc6040547879da447a8d9ce265
Jirattikal42 <60840444+Jirattikal42@users.noreply.github.com>
2020-08-30 14:55:22 +0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+
+var word = ""; 
+
+
+var rand = Math.floor(Math.random() * 4);
+switch (rand) {
+  case 0:  
+    word = "table";
+    break;
+  case 1: 
+    word = "chair";
+    break;
+  case 2:  
+    word = "glass";
+    break;
+  case 3:      
+    word = "paper";
+    break;
+}
+
 function App() {
   return (
     <div>
